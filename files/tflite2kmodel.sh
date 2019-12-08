f39372da1a890e7a787e1c3baa05809b43175a28
Joakim Flink <39080802+abbjoafli@users.noreply.github.com>
2019-12-08 05:36:59 +0100

    Update tflite2kmodel.sh

--- files/tflite2kmodel.sh
+++ files/tflite2kmodel.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 echo "uasge: ./tflite2kmodel.sh xxx.tflite"
-# name=`echo $1 | cut -d '.' -f 1`
-# name=$name.kmodel
-# /content/Maix_Toolbox/ncc/ncc -i tflite -o k210model --dataset images $1 ./$name
-/content/Maix_Toolbox/ncc/ncc compile /content/Yolo-digit-detector/model.tflite /content/Maix_Toolbox/model.kmodel -i tflite -o kmodel -t k210 --dataset /content/Maix_Toolbox/images
-# /content/Maix_Toolbox/ncc/ncc compile model.tflite model.kmodel -i tflite -o kmodel -t k210 --dataset images
+name=`echo $1 | cut -d '.' -f 1`
+name=$name.kmodel
+/content/Maix_Toolbox/ncc/ncc -i tflite -o k210model --dataset /content/Maix_Toolbox/images $1 /$name
+#Nedan är om man anvädner ncc 2.0, funkar inte.
+# /content/Maix_Toolbox/ncc/ncc compile /content/Yolo-digit-detector/model.tflite /content/Maix_Toolbox/model.kmodel -i tflite -o kmodel -t k210 --dataset
+
